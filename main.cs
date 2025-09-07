9639a3c166d2b0fda11ace1d6abf251111c8e40b
randomcat962codes <landonp1287913+github@gmail.com>
2025-09-06 20:15:41 -0500

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,9 @@
+class Program
+{
+  public static void Main(string[] args)
+  {
+    Console.WriteLine("Hello World!");
+    Console.Write("Press enter to close the program > ");
+    Console.ReadLine();
+  }
+}
